15dbc510ab6c6fab12512acbff3ed45c95cbcadd
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-03-11 12:28:48 +0100

    Have a problem

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const RESULT = [];
+  const STATE_HISTORY = {};
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      const { extraData } = actions[i];
+
+      Object.assign(STATE_HISTORY, state, extraData);
+      RESULT.push(STATE_HISTORY);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      const REMOVE_HISTORY = { ...state };
+
+      for (const key of actions[i].keysToRemove) {
+        delete REMOVE_HISTORY[key];
+      }
+      RESULT.push(REMOVE_HISTORY);
+    }
+
+    if (actions[i].type === 'clear') {
+      const CLEAR_HISTORY = {};
+
+      RESULT.push(CLEAR_HISTORY);
+    }
+  }
+
+  return RESULT;
 }
 
 module.exports = transformStateWithClones;
