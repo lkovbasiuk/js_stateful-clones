74f34933b2909b7699c1de8a6842de5d8d445d36
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-03-11 18:37:29 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,32 +6,57 @@
  *
  * @return {Object[]}
  */
+// function transformStateWithClones(state, actions) {
+//   const RESULT = [];
+//   const STATE_HISTORY = {};
+
+//   for (let i = 0; i < actions.length; i++) {
+//     if (actions[i].type === 'addProperties') {
+//       const { extraData } = actions[i];
+
+//       Object.assign(STATE_HISTORY, state, extraData);
+//       RESULT.push(STATE_HISTORY);
+//     }
+
+//     if (actions[i].type === 'removeProperties') {
+//       const REMOVE_HISTORY = { ...state };
+
+//       for (const key of actions[i].keysToRemove) {
+//         delete REMOVE_HISTORY[key];
+//       }
+//       RESULT.push(REMOVE_HISTORY);
+//     }
+
+//     if (actions[i].type === 'clear') {
+//       const CLEAR_HISTORY = {};
+
+//       RESULT.push(CLEAR_HISTORY);
+//     }
+//   }
+
+//   return RESULT;
+// }
+
 function transformStateWithClones(state, actions) {
   const RESULT = [];
-  const STATE_HISTORY = {};
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      const { extraData } = actions[i];
+  let STATE_HISTORY = { ...state };
 
-      Object.assign(STATE_HISTORY, state, extraData);
-      RESULT.push(STATE_HISTORY);
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      STATE_HISTORY = {};
     }
 
-    if (actions[i].type === 'removeProperties') {
-      const REMOVE_HISTORY = { ...state };
+    if (action.type === 'addProperties') {
+      Object.assign(STATE_HISTORY, action.extraData);
+    }
 
-      for (const key of actions[i].keysToRemove) {
-        delete REMOVE_HISTORY[key];
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete STATE_HISTORY[key];
       }
-      RESULT.push(REMOVE_HISTORY);
     }
-
-    if (actions[i].type === 'clear') {
-      const CLEAR_HISTORY = {};
-
-      RESULT.push(CLEAR_HISTORY);
-    }
+    RESULT.push({ ...STATE_HISTORY });
   }
 
   return RESULT;
